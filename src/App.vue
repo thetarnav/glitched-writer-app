70a177c8cd08c667d1952a44d3dcbb0caf950be6
Damian Tarnawski @thetarnav <gthetarnav@gmail.com>
2021-04-16 17:47:34 +0200

    vue project init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>Hello World</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+	name: 'App',
+	components: {
+		// HelloWorld,
+	},
+})
+</script>
+
+<style src="./scss/base.scss" lang="scss" />
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// @ts-ignore
+module.exports = {
+	// purge: [],
+	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
